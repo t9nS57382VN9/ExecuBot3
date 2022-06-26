766498a8d050d6533beb82e0b74eba1906fc7915
t9nS57382VN9 <108196701+t9nS57382VN9@users.noreply.github.com>
2022-06-26 05:50:40 +0000

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -112,15 +112,15 @@ serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-	if self.path == "requestJoin":
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
-		self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-		self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-		self.wfile.write(bytes("<body>", "utf-8"))
-		self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-		self.wfile.write(bytes("</body></html>", "utf-8"))
+        if self.path == "requestJoin":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
